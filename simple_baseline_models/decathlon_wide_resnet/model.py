95936b150ed9a7ec2d69c7695038ac999e482c71
nick11roberts <nick11roberts@cs.wisc.edu>
2022-07-25 20:48:18 -0500

    Update WRN baseline

--- simple_baseline_models/decathlon_wide_resnet/model.py
+++ simple_baseline_models/decathlon_wide_resnet/model.py
@@ -1,8 +1,6 @@
 """An example of code submission for the AutoML Decathlon challenge.
 
-It implements 3 compulsory methods ('__init__', 'train' and 'test') and
-an attribute 'done_training' for indicating if the model will not proceed more
-training due to convergence or limited time budget.
+It implements 3 compulsory methods ('__init__', 'train' and 'test'). 
 
 To create a valid submission, zip model.py and metadata together with other necessary files
 such as tasks_to_run.yaml, Python modules/packages, pre-trained weights, etc. The final zip file
@@ -30,14 +28,13 @@ np.random.seed(42)
 torch.manual_seed(1)
 
 # PyTorch Model class
-class TorchModel(nn.Module):
+class WideResNet(nn.Module):
     """
     Defines a module that will be created in '__init__' of the 'Model' class below, and will be used for training and predictions.
     """
 
     def __init__(self, input_shape, output_dim):
-        """a simple linear model"""
-        super(TorchModel, self).__init__()
+        super(WideResNet, self).__init__()
 
         fc_size = np.prod(input_shape)
         print("input_shape, fc_size", input_shape, fc_size)
@@ -60,7 +57,6 @@ class Model:
               ingestion/dev_datasets.py
         """
         # Attribute necessary for ingestion program to stop evaluation process
-        self.done_training = False
         self.metadata_ = metadata
 
         # Getting details of the data from meta data
@@ -82,14 +78,12 @@ class Model:
             "Device Found = ", self.device, "\nMoving Model and Data into the device..."
         )
 
-        # TODO
         self.input_shape = (sequence_size, channel, row_count, col_count)
         print("\n\nINPUT SHAPE = ", self.input_shape)
 
         # getting an object for the PyTorch Model class for Model Class
         # use CUDA if available
-        # TODO
-        depth = 16  # TODO increase to 40
+        depth = 16
         spacetime_dims = np.count_nonzero(np.array(self.input_shape)[[0, 2, 3]] != 1)
         logger.info(f"Using WRN of dimension {spacetime_dims}")
         if spacetime_dims == 1:
@@ -154,13 +148,9 @@ class Model:
         self.cumulated_num_steps = 0
         self.estimated_time_per_step = None
         self.total_test_time = 0
-        self.cumulated_num_tests = 0
         self.estimated_time_test = None
         self.trained = False
-
-        # PYTORCH
-        # Critical number for early stopping
-        self.num_epochs_we_want_to_train = 200
+        self.training_epochs = 200
 
         # no of examples at each step/batch
         self.train_batch_size = 128
@@ -206,18 +196,6 @@ class Model:
         process. So your `train` method should be able to handle repeated calls and
         hopefully improve your model performance after each call.
 
-        ****************************************************************************
-        ****************************************************************************
-        IMPORTANT: the loop of calling `train` and `test` will only run if
-            self.done_training = False
-          (the corresponding code can be found in ingestion.py, search
-          'M.done_training')
-          Otherwise, the loop will go on until the time budget is used up. Please
-          pay attention to set self.done_training = True when you think the model is
-          converged or when there is not enough time for next round of training.
-        ****************************************************************************
-        ****************************************************************************
-
         Args:
           dataset: a `DecathlonDataset` object. Each of its examples is of the form
                 (example, labels)
@@ -234,67 +212,19 @@ class Model:
               budget. If remaining_time_budget is None, no time budget is imposed.
         """
 
-        steps_to_train = self.get_steps_to_train(remaining_time_budget)
-        if steps_to_train <= 0:
-            logger.info(
-                "Not enough time remaining for training. "
-                + "Estimated time for training per step: {:.2f}, ".format(
-                    self.estimated_time_per_step
-                )
-                + "but remaining time budget is: {:.2f}. ".format(remaining_time_budget)
-                + "Skipping..."
-            )
-            self.done_training = True
-        else:
-            msg_est = ""
-            if self.estimated_time_per_step:
-                msg_est = "estimated time for this: " + "{:.2f} sec.".format(
-                    steps_to_train * self.estimated_time_per_step
-                )
-            logger.info(
-                "Begin training for another {} steps...{}".format(
-                    steps_to_train, msg_est
-                )
+        # If PyTorch dataloader for training set doesn't already exist,
+        # get the train dataloader
+        if not hasattr(self, "trainloader"):
+            self.trainloader = self.get_dataloader(
+                dataset,
+                self.train_batch_size,
+                "train",
             )
 
-            # If PyTorch dataloader for training set doen't already exists, get the train dataloader
-            if not hasattr(self, "trainloader"):
-                self.trainloader = self.get_dataloader(
-                    dataset,
-                    self.train_batch_size,
-                    "train",
-                )
+        # Training loop
+        logger.info(f"epochs to train {self.training_epochs}")
 
-            train_start = time.time()
-
-            # Training loop
-            steps_to_train = len(self.trainloader) * self.num_epochs_we_want_to_train
-            logger.info(f"steps to train {steps_to_train}")
-            logger.info(f"epochs to train {self.num_epochs_we_want_to_train}")
-
-            self.trainloop(self.criterion, self.optimizer, steps=steps_to_train)
-            train_end = time.time()
-
-            # Update for time budget managing
-            # TODO
-            train_duration = train_end - train_start
-            self.total_train_time += train_duration
-            self.cumulated_num_steps += steps_to_train
-            self.estimated_time_per_step = (
-                self.total_train_time / self.cumulated_num_steps
-            )
-            logger.info(
-                "{} steps trained. {:.2f} sec used. ".format(
-                    steps_to_train, train_duration
-                )
-                + "Now total steps trained: {}. ".format(self.cumulated_num_steps)
-                + "Total time used for training: {:.2f} sec. ".format(
-                    self.total_train_time
-                )
-                + "Current estimated time per step: {:.2e} sec.".format(
-                    self.estimated_time_per_step
-                )
-            )
+        self.trainloop(self.criterion, self.optimizer, epochs=self.training_epochs)
 
     def test(self, dataset, remaining_time_budget=None):
         """Test this algorithm on the Pytorch dataloader.
@@ -307,30 +237,7 @@ class Model:
               set and `output_dim` is the number of labels to be predicted. The
               values should be binary or in the interval [0,1].
         """
-        if self.done_training:
-            return None
-
-        if self.choose_to_stop_early():
-            logger.info("Oops! Choose to stop early for next call!")
-            self.done_training = True
         test_begin = time.time()
-        if (
-            remaining_time_budget
-            and self.estimated_time_test
-            and self.estimated_time_test > remaining_time_budget
-        ):
-            logger.info(
-                "Not enough time for test. "
-                + "Estimated time for test: {:.2e}, ".format(self.estimated_time_test)
-                + "But remaining time budget is: {:.2f}. ".format(remaining_time_budget)
-                + "Stop train/predict process by returning None."
-            )
-            return None
-
-        msg_est = ""
-        if self.estimated_time_test:
-            msg_est = "estimated time: {:.2e} sec.".format(self.estimated_time_test)
-        logger.info("Begin testing..." + msg_est)
 
         if not hasattr(self, "testloader"):
             self.testloader = self.get_dataloader(
@@ -346,24 +253,19 @@ class Model:
         # Update some variables for time management
         test_duration = test_end - test_begin
         self.total_test_time += test_duration
-        self.cumulated_num_tests += 1
-        self.estimated_time_test = self.total_test_time / self.cumulated_num_tests
         logger.info(
             "[+] Successfully made one prediction. {:.2f} sec used. ".format(
                 test_duration
             )
             + "Total time used for testing: {:.2f} sec. ".format(self.total_test_time)
-            + "Current estimated time for test: {:.2e} sec.".format(
-                self.estimated_time_test
-            )
         )
         return predictions
 
-    ##############################################################################
-    #### Above 3 methods (__init__, train, test) should always be implemented ####
-    ##############################################################################
+    ############################################################################
+    ### Above 3 methods (__init__, train, test) should always be implemented ###
+    ############################################################################
 
-    def trainloop(self, criterion, optimizer, steps):
+    def trainloop(self, criterion, optimizer, epochs):
         """Training loop with no of given steps
         Args:
           criterion: PyTorch Loss function
@@ -374,63 +276,22 @@ class Model:
           None, updates the model parameters
         """
         self.model.train()
-        data_iterator = iter(self.trainloader)
-        for _ in tqdm(range(steps)):
-            try:
-                images, labels = next(data_iterator)
-            except StopIteration:
-                data_iterator = iter(self.trainloader)
-                images, labels = next(data_iterator)
+        for _ in tqdm(range(epochs), desc="Epochs trained", position=0):
+            for x, y in tqdm(
+                self.trainloader, desc="Batches this epoch", position=1, leave=False
+            ):
+                x = x.float().to(self.device)
+                y = y.float().to(self.device)
+                optimizer.zero_grad()
 
-            images = images.float().to(self.device)
-            # labels = nn.functional.one_hot(labels, num_classes=18)
-            labels = labels.float().to(self.device)
-            optimizer.zero_grad()
+                logits = self.model(x)
+                loss = criterion(logits, y.reshape(y.shape[0], -1))
 
-            logits = self.model(images)
-            # print('logits.shape', logits.shape)
-            # print('labels', labels.shape)
-            # FIXME make sure that things are correctly reshaped...
+                if hasattr(self, "scheduler"):
+                    self.scheduler.step(loss)
 
-            loss = criterion(logits, labels.reshape(labels.shape[0], -1))
-            if hasattr(self, "scheduler"):
-                self.scheduler.step(loss)
-            loss.backward()
-            optimizer.step()
-
-    def get_steps_to_train(self, remaining_time_budget):
-        """Get number of steps for training according to `remaining_time_budget`.
-
-        The strategy is:
-          1. If no training is done before, train for 10 steps (ten batches);
-          2. Otherwise, estimate training time per step and time needed for test,
-             then compare to remaining time budget to compute a potential maximum
-             number of steps (max_steps) that can be trained within time budget;
-          3. Choose a number (steps_to_train) between 0 and max_steps and train for
-             this many steps. Double it each time.
-        """
-        if not remaining_time_budget:  # This is never true in the competition anyway
-            remaining_time_budget = 1200  # if no time limit is given, set to 20min
-
-        if not self.estimated_time_per_step:
-            steps_to_train = 100
-        else:
-            if self.estimated_time_test:
-                tentative_estimated_time_test = self.estimated_time_test
-            else:
-                tentative_estimated_time_test = 50  # conservative estimation for test
-            max_steps = int(
-                (remaining_time_budget - tentative_estimated_time_test)
-                / self.estimated_time_per_step
-            )
-            max_steps = max(max_steps, 1)
-            if self.cumulated_num_tests < np.log(max_steps) / np.log(2):
-                steps_to_train = int(
-                    2**self.cumulated_num_tests
-                )  # Double steps_to_train after each test
-            else:
-                steps_to_train = 0
-        return steps_to_train
+                loss.backward()
+                optimizer.step()
 
     def testloop(self, dataloader):
         """
@@ -443,12 +304,12 @@ class Model:
         preds = []
         with torch.no_grad():
             self.model.eval()
-            for images, _ in iter(dataloader):
+            for x, _ in iter(dataloader):
                 if torch.cuda.is_available():
-                    images = images.float().cuda()
+                    x = x.float().cuda()
                 else:
-                    images = images.float()
-                logits = self.model(images)
+                    x = x.float()
+                logits = self.model(x)
 
                 # Choose correct prediction type
                 if self.metadata_.get_task_type() == "continuous":
@@ -464,20 +325,6 @@ class Model:
 
         preds = np.vstack(preds)
         return preds
-
-    def choose_to_stop_early(self):
-        """The criterion to stop further training (thus finish train/predict
-        process).
-        """
-        # return self.cumulated_num_tests > 10 # Limit to make 10 predictions
-        # return np.random.rand() < self.early_stop_proba
-        batch_size = self.train_batch_size
-        num_examples = self.metadata_.size()
-        num_epochs = self.cumulated_num_steps * batch_size / num_examples
-        logger.info("Model already trained for {} epochs.".format(num_epochs))
-        return (
-            num_epochs > self.num_epochs_we_want_to_train
-        )  # Train for at least certain number of epochs then stop
 
 
 def get_logger(verbosity_level):
